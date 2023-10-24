297170681cab867439a58f20cdbe99fe3dd6bb9c
henryanandsr <13521004@std.stei.itb.ac.id>
2023-10-24 20:46:04 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import { Routes, Route} from "react-router-dom";
+// import from login page
+import Login from "./pages/Login/Login";
+function App() {
+  return (
+      
+  );
+}
+
+export default App;
