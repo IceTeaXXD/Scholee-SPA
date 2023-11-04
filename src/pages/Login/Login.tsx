29a99a711b342c280175f7a0c1ffad7d7a73f513
henryanandsr <13521004@std.stei.itb.ac.id>
2023-11-04 12:53:55 +0700

    fix : login page with auth

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useRef, useState, useEffect} from 'react';
 import {
   Flex,
   Heading,
@@ -9,13 +9,50 @@ import {
   Switch,
   useColorMode,
   useColorModeValue,
+  Box,
 } from '@chakra-ui/react';
-import { Link } from 'react-router-dom'; 
+import { Link, useNavigate } from 'react-router-dom'; 
+import axios from '../../api/axios';
+import {setAuthToken} from "../../utils/auth";
+
+const LOGIN = '/api/login';
 const Login = () => {
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+
   const { colorMode, toggleColorMode } = useColorMode();
   const formBackground = useColorModeValue('blue.100', 'blue.700');
   const buttonColor = useColorModeValue('blue.400', 'blue.300');
   const textColor = useColorModeValue('gray.700', 'gray.100');
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log(email, password);
+    try {
+      const res = await axios.post(LOGIN, {
+        email,
+        password,
+      });
+      if (res.status === 200) {
+        console.log("Login Success")
+        // console.log(res.data);
+        setAuthToken(res.data.accessToken);
+        if (res.data.role ==="university") {
+          navigate("/home");
+        } else {
+          navigate("/home")
+          // TODO : bedain antara uni dan org
+        }
+      }
+      // navigate('/dashboard');
+    } catch {
+      setErrMsg('Invalid email or password');
+    }
+  }
+
   return (
     <Flex h="100vh" alignItems="center" justifyContent="center">
       <Flex
@@ -26,6 +63,16 @@ const Login = () => {
         boxShadow="lg"
       >
         <Heading mb={6}>Log In</Heading>
+        <Box
+          color="red.500"
+          display={errMsg ? 'block' : 'none'}
+          mb={3}
+          fontWeight="bold"
+          fontSize="sm"
+          aria-live="assertive"
+        >
+          {errMsg}
+        </Box>
         <FormControl id="email" isRequired>
           <FormLabel>Email</FormLabel>
           <Input
@@ -33,6 +80,9 @@ const Login = () => {
             type="text"
             variant="filled"
             mb={3}
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </FormControl>
         <FormControl id="password" isRequired>
@@ -42,10 +92,13 @@ const Login = () => {
             type="password"
             variant="filled"
             mb={3}
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </FormControl>
         <Link to="/dashboard">
-          <Button bg={buttonColor} mb={8}>
+          <Button bg={buttonColor} mb={8} onClick={handleSubmit}>
             Log In
           </Button>
         </Link>
