962e97f24c9a162ab52f3e329644f877f2dc4503
henryanandsr <13521004@std.stei.itb.ac.id>
2023-11-07 16:50:41 +0700

    feat : axios private route

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -3,3 +3,9 @@ import axios from "axios"
 export default axios.create({
     baseURL: "http://localhost:5001"
 })
+
+export const axiosPrivate = axios.create({
+    baseURL: "http://localhost:5001",
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true
+});
