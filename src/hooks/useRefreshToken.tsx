71df3870d0a2c807deb9295888f97a0f96ceec53
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-11-10 16:29:44 +0700

    chore: use env url

--- src/hooks/useRefreshToken.tsx
+++ src/hooks/useRefreshToken.tsx
@@ -3,7 +3,7 @@ import axios from '../api/axios';
 const useRefreshToken = () => {
     const refresh = async () => {
         try {
-            const response = await axios.get('http://localhost:5001/api/refresh');
+            const response = await axios.get(process.env.REACT_APP_API_URL + "/api/refresh");
             return response.data;
         } catch (error) {
             console.error("An error occurred while refreshing the token:", error);
