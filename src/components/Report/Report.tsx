4aa4ea80cf28b6a7a9445ff5f9edfb0ad1eb0007
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-11-15 21:29:21 +0700

    fix: fetch on report

--- src/components/Report/Report.tsx
+++ src/components/Report/Report.tsx
@@ -1,9 +1,37 @@
-import { Box, Heading, TableContainer, Table, Th, Thead, Tr, Tbody, Td, Stack, Input, InputGroup, InputLeftElement, MenuItemOption, MenuOptionGroup, Menu, Button, MenuButton, MenuList, HStack, Icon } from "@chakra-ui/react"
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-import { handleGetInfo } from "../../utils/auth";
-import { ArrowBackIcon, ArrowForwardIcon, ChevronDownIcon, Search2Icon } from "@chakra-ui/icons";
-import { FaFilter } from "react-icons/fa";
+/* eslint-disable react-hooks/exhaustive-deps */
+import {
+  Box,
+  Heading,
+  TableContainer,
+  Table,
+  Th,
+  Thead,
+  Tr,
+  Tbody,
+  Td,
+  Stack,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  MenuItemOption,
+  MenuOptionGroup,
+  Menu,
+  Button,
+  MenuButton,
+  MenuList,
+  HStack,
+  Icon
+} from "@chakra-ui/react"
+import axios from "axios"
+import React, { useState, useEffect } from "react"
+import { handleGetInfo } from "../../utils/auth"
+import {
+  ArrowBackIcon,
+  ArrowForwardIcon,
+  ChevronDownIcon,
+  Search2Icon
+} from "@chakra-ui/icons"
+import { FaFilter } from "react-icons/fa"
 import { debounce, get } from "lodash"
 
 const Report: React.FC = () => {
@@ -13,66 +41,58 @@ const Report: React.FC = () => {
     email: "",
     role: ""
   });
-
-  const [students, setStudents] = useState([])
+  
+  const [userId, setUserId] = useState(0);
+  const [students, setStudents] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(5);
   const [search, setSearch] = useState("");
-  const [numberOfPages, setNumberOfPages] = useState(0)
-  const MAX_PAGE_BUTTONS = 3
-
+  const [numberOfPages, setNumberOfPages] = useState(0);
+  const MAX_PAGE_BUTTONS = 3;
+  
   const fetchStudents = async () => {
-    const url = new URL(process.env.REACT_APP_API_URL + "/api/university/stats/" + userInfo.user_id)
-    const params = new URLSearchParams()
-    params.append("name", search)
-    params.append("itemsperpage", String(itemsPerPage))
-    params.append("currentPage", String(currentPage))
-    url.search = (params.toString())
-    const response = await axios.get(url.toString())
-    const students = await response.data
-    setNumberOfPages(Math.ceil(students.data.total / itemsPerPage || 100))
-    setStudents(students.data.data)
-  }
+    const url = new URL(
+      process.env.REACT_APP_API_URL + "/api/university/stats/" + userId
+    );
+    const params = new URLSearchParams();
+    params.append("name", search);
+    params.append("itemsperpage", String(itemsPerPage));
+    params.append("currentPage", String(currentPage));
+    console.log(params.toString());
+    url.search = params.toString();
+    const response = await axios.get(url.toString());
+    const students = await response.data;
+    setNumberOfPages(Math.ceil(students.data.total / itemsPerPage || 100));
+    setStudents(students.data.data);
+  };
   
-  const getInfo = async () => {
-    const response = await handleGetInfo();
-    setUserInfo({
-      user_id: response?.data.user_id,
-      name: response?.data.name,
-      email: response?.data.email,
-      role: response?.data.roles
-    });
-  }
-  
-  getInfo()
-  const debounceFetch = debounce(fetchStudents, 500)
+  const debounceFetch = debounce(fetchStudents, 500);
   
   useEffect(() => {
-    fetchStudents()
-  }, [userInfo.user_id])
-
-  useEffect(() => {
-    const delayedFetch = () => {
-      debounceFetch()
-    }
-    setCurrentPage(1)
-    const delay = setTimeout(delayedFetch, 500)
-    return () => clearTimeout(delay)
-  }, [search])
-
-  useEffect(() => {
-    setCurrentPage(1)
-    fetchStudents()
-  }, [itemsPerPage])
+    const getInfoAndFetchStudents = async () => {
+      const response = await handleGetInfo();
+      setUserInfo({
+        user_id: response?.data.user_id,
+        name: response?.data.name,
+        email: response?.data.email,
+        role: response?.data.roles
+      });
   
-  useEffect(() => {
-    fetchStudents()
-  }, [currentPage])
+      if (userId === 0 && response?.data.user_id !== 0) {
+        setUserId(response?.data.user_id);
+      }
   
-  const startPage =
-  currentPage <= Math.floor(MAX_PAGE_BUTTONS / 2)
-    ? 1
-    : Math.max(1, currentPage - Math.floor(MAX_PAGE_BUTTONS / 2))
+      setCurrentPage(1);
+      debounceFetch();
+    };
+  
+    getInfoAndFetchStudents();
+  }, [search, itemsPerPage, currentPage, userId]);
+  
+    const startPage =
+    currentPage <= Math.floor(MAX_PAGE_BUTTONS / 2)
+      ? 1
+      : Math.max(1, currentPage - Math.floor(MAX_PAGE_BUTTONS / 2))
 
   const endPage =
     startPage + MAX_PAGE_BUTTONS - 1 <= numberOfPages
@@ -81,72 +101,72 @@ const Report: React.FC = () => {
 
   return (
     <Box p="12">
-        <Heading size="sm" as="h1" mb="6" fontSize={48}>
-          List of Students
-        </Heading>
-        <Stack>
-          {/* Search Bar */}
-          <InputGroup borderRadius={10} size="sm">
-            <InputLeftElement
-              pointerEvents="none"
-              children={<Search2Icon color="gray.600" />}
-            />
-            <Input
-              type="text"
-              borderRadius={10}
-              placeholder="Search..."
-              border="1px solid #949494"
-              onChange={(event) => setSearch(event.target.value)}
-            />
-          </InputGroup>
+      <Heading size="sm" as="h1" mb="6" fontSize={48}>
+        List of Students
+      </Heading>
+      <Stack>
+        {/* Search Bar */}
+        <InputGroup borderRadius={10} size="sm">
+          <InputLeftElement
+            pointerEvents="none"
+            children={<Search2Icon color="gray.600" />}
+          />
+          <Input
+            type="text"
+            borderRadius={10}
+            placeholder="Search..."
+            border="1px solid #949494"
+            onChange={(event) => setSearch(event.target.value)}
+          />
+        </InputGroup>
 
-          <Menu closeOnSelect={false}>
-            <MenuButton
-              as={Button}
-              leftIcon={<FaFilter />}
-              rightIcon={<ChevronDownIcon />}
-              size={"md"}
-              minWidth={"150px"}
+        <Menu closeOnSelect={false}>
+          <MenuButton
+            as={Button}
+            leftIcon={<FaFilter />}
+            rightIcon={<ChevronDownIcon />}
+            size={"md"}
+            minWidth={"150px"}
+          >
+            Filter
+          </MenuButton>
+          <MenuList minWidth="240px">
+            {/* Items per Page */}
+            <MenuOptionGroup
+              title="Items per Page"
+              type="radio"
+              defaultValue="5"
+              onChange={(value) => setItemsPerPage(Number(value))}
             >
-              Filter
-            </MenuButton>
-            <MenuList minWidth="240px">
-              {/* Items per Page */}
-              <MenuOptionGroup
-                title="Items per Page"
-                type="radio"
-                defaultValue="5"
-                onChange={(value) => setItemsPerPage(Number(value))}
-              >
-                <MenuItemOption value="5">5</MenuItemOption>
-                <MenuItemOption value="10">10</MenuItemOption>
-                <MenuItemOption value="15">15</MenuItemOption>
-                <MenuItemOption value="20">20</MenuItemOption>
-              </MenuOptionGroup>
-            </MenuList>
-          </Menu>
-        </Stack>
-        <Box>
-          <TableContainer>
-            <Table variant="striped" colorScheme="facebook">
-              <Thead>
-                <Tr>
-                  <Th>Student Name</Th>
-                  <Th>Email</Th>
+              <MenuItemOption value="5">5</MenuItemOption>
+              <MenuItemOption value="10">10</MenuItemOption>
+              <MenuItemOption value="15">15</MenuItemOption>
+              <MenuItemOption value="20">20</MenuItemOption>
+            </MenuOptionGroup>
+          </MenuList>
+        </Menu>
+      </Stack>
+      <Box>
+        <TableContainer>
+          <Table variant="striped" colorScheme="facebook">
+            <Thead>
+              <Tr>
+                <Th>Student Name</Th>
+                <Th>Email</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {students.map((student: any) => (
+                <Tr key={student.applicant_email}>
+                  <Td>{student.applicant_name}</Td>
+                  <Td>{student.applicant_email}</Td>
                 </Tr>
-              </Thead>
-              <Tbody>
-                {students.map((student: any) => (
-                  <Tr key={student.applicant_email}>
-                    <Td>{student.applicant_name}</Td>
-                    <Td>{student.applicant_email}</Td>
-                  </Tr>
-                ))}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </Box>
-        {students.length > 0 && (
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
+      {students.length > 0 && (
         <HStack spacing="24px" justifyContent="center" mt={"20px"}>
           {/* Prev Button */}
           <Button
@@ -182,7 +202,7 @@ const Report: React.FC = () => {
           </Button>
         </HStack>
       )}
-    </Box> 
+    </Box>
   )
 }
 
