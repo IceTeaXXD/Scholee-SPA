21ac8f875f547ab0282459edb5fd41e9399b906f
Matthew Mahendra <13521007@std.stei.itb.ac.id>
2023-11-15 20:59:40 +0700

    feat: University Students Pagination (error on first load)

--- src/components/Report/Report.tsx
+++ src/components/Report/Report.tsx
@@ -1,9 +1,12 @@
-import { Flex, Box, Text, Heading, TableContainer, Table, Th, Thead, Tr, Tbody, Td } from "@chakra-ui/react"
+import { Box, Heading, TableContainer, Table, Th, Thead, Tr, Tbody, Td, Stack, Input, InputGroup, InputLeftElement, MenuItemOption, MenuOptionGroup, Menu, Button, MenuButton, MenuList, HStack, Icon } from "@chakra-ui/react"
 import axios from "axios";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { handleGetInfo } from "../../utils/auth";
+import { ArrowBackIcon, ArrowForwardIcon, ChevronDownIcon, Search2Icon } from "@chakra-ui/icons";
+import { FaFilter } from "react-icons/fa";
+import { debounce, get } from "lodash"
 
-const Report = () => {
+const Report: React.FC = () => {
   const [userInfo, setUserInfo] = useState({
     user_id: 0,
     name: "",
@@ -12,36 +15,117 @@ const Report = () => {
   });
 
   const [students, setStudents] = useState([])
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [search, setSearch] = useState("");
+  const [numberOfPages, setNumberOfPages] = useState(0)
+  const MAX_PAGE_BUTTONS = 3
+
+  const fetchStudents = async () => {
+    const url = new URL(process.env.REACT_APP_API_URL + "/api/university/stats/" + userInfo.user_id)
+    const params = new URLSearchParams()
+    params.append("name", search)
+    params.append("itemsperpage", String(itemsPerPage))
+    params.append("currentPage", String(currentPage))
+    url.search = (params.toString())
+    const response = await axios.get(url.toString())
+    const students = await response.data
+    setNumberOfPages(Math.ceil(students.data.total / itemsPerPage || 100))
+    setStudents(students.data.data)
+  }
+  
+  const getInfo = async () => {
+    const response = await handleGetInfo();
+    setUserInfo({
+      user_id: response?.data.user_id,
+      name: response?.data.name,
+      email: response?.data.email,
+      role: response?.data.roles
+    });
+  }
+  
+  getInfo()
+  const debounceFetch = debounce(fetchStudents, 500)
+  
+  useEffect(() => {
+    fetchStudents()
+  }, [userInfo.user_id])
 
   useEffect(() => {
-    const getInfo = async () => {
-      try {
-        const response = await handleGetInfo();
-        setUserInfo({
-          user_id: response?.data.user_id,
-          name: response?.data.name,
-          email: response?.data.email,
-          role: response?.data.roles
-        });
+    const delayedFetch = () => {
+      debounceFetch()
+    }
+    setCurrentPage(1)
+    const delay = setTimeout(delayedFetch, 500)
+    return () => clearTimeout(delay)
+  }, [search])
 
-        const students = await axios.get(
-          process.env.REACT_APP_API_URL + "/api/university/stats/" + response?.data.user_id
-        );
-
-        setStudents(students.data.data)
-      } catch (error) {
-        console.error("Error in useEffect:", error);
-      }
-    };
-
-    getInfo();
-  }, []);
+  useEffect(() => {
+    setCurrentPage(1)
+    fetchStudents()
+  }, [itemsPerPage])
   
+  useEffect(() => {
+    fetchStudents()
+  }, [currentPage])
+  
+  const startPage =
+  currentPage <= Math.floor(MAX_PAGE_BUTTONS / 2)
+    ? 1
+    : Math.max(1, currentPage - Math.floor(MAX_PAGE_BUTTONS / 2))
+
+  const endPage =
+    startPage + MAX_PAGE_BUTTONS - 1 <= numberOfPages
+      ? startPage + MAX_PAGE_BUTTONS - 1
+      : numberOfPages
+
   return (
     <Box p="12">
         <Heading size="sm" as="h1" mb="6" fontSize={48}>
           List of Students
         </Heading>
+        <Stack>
+          {/* Search Bar */}
+          <InputGroup borderRadius={10} size="sm">
+            <InputLeftElement
+              pointerEvents="none"
+              children={<Search2Icon color="gray.600" />}
+            />
+            <Input
+              type="text"
+              borderRadius={10}
+              placeholder="Search..."
+              border="1px solid #949494"
+              onChange={(event) => setSearch(event.target.value)}
+            />
+          </InputGroup>
+
+          <Menu closeOnSelect={false}>
+            <MenuButton
+              as={Button}
+              leftIcon={<FaFilter />}
+              rightIcon={<ChevronDownIcon />}
+              size={"md"}
+              minWidth={"150px"}
+            >
+              Filter
+            </MenuButton>
+            <MenuList minWidth="240px">
+              {/* Items per Page */}
+              <MenuOptionGroup
+                title="Items per Page"
+                type="radio"
+                defaultValue="5"
+                onChange={(value) => setItemsPerPage(Number(value))}
+              >
+                <MenuItemOption value="5">5</MenuItemOption>
+                <MenuItemOption value="10">10</MenuItemOption>
+                <MenuItemOption value="15">15</MenuItemOption>
+                <MenuItemOption value="20">20</MenuItemOption>
+              </MenuOptionGroup>
+            </MenuList>
+          </Menu>
+        </Stack>
         <Box>
           <TableContainer>
             <Table variant="striped" colorScheme="facebook">
@@ -53,7 +137,7 @@ const Report = () => {
               </Thead>
               <Tbody>
                 {students.map((student: any) => (
-                  <Tr key={student}>
+                  <Tr key={student.applicant_email}>
                     <Td>{student.applicant_name}</Td>
                     <Td>{student.applicant_email}</Td>
                   </Tr>
@@ -62,8 +146,43 @@ const Report = () => {
             </Table>
           </TableContainer>
         </Box>
-    </Box>
-    
+        {students.length > 0 && (
+        <HStack spacing="24px" justifyContent="center" mt={"20px"}>
+          {/* Prev Button */}
+          <Button
+            onClick={() => setCurrentPage(currentPage - 1)}
+            isDisabled={currentPage === 1} // disable if on first page
+          >
+            <Icon as={ArrowBackIcon} />
+          </Button>
+
+          {/* Page Buttons */}
+          {Array.from(
+            { length: endPage - startPage + 1 },
+            (_, index) => startPage + index
+          ).map((pageNumber) => (
+            <Button
+              variant="outline"
+              colorScheme={currentPage === pageNumber ? "teal" : "gray"}
+              borderRadius="full"
+              w="40px"
+              key={pageNumber}
+              onClick={() => setCurrentPage(pageNumber)}
+            >
+              {pageNumber}
+            </Button>
+          ))}
+
+          {/* Next Button */}
+          <Button
+            onClick={() => setCurrentPage(currentPage + 1)}
+            isDisabled={currentPage === numberOfPages}
+          >
+            <Icon as={ArrowForwardIcon} />
+          </Button>
+        </HStack>
+      )}
+    </Box> 
   )
 }
 
