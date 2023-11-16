7354f0f123b1f0c821327162f947a7f9c82f13cb
Matthew Mahendra <13521007@std.stei.itb.ac.id>
2023-11-16 10:05:43 +0700

    feat: Scholarship Acceptance (error: Data not loaded on first load)

--- src/components/Acceptance/Acceptance.tsx
+++ src/components/Acceptance/Acceptance.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Button,
   Icon,
@@ -27,122 +27,200 @@ import {
 } from "@chakra-ui/react";
 import { Search2Icon, CheckIcon, CloseIcon } from "@chakra-ui/icons";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Acceptance = () => {
-  const { scholarshipid } = useParams();
-  const toast = useToast();
-  const { isOpen: acceptModalIsOpen, onOpen: onAcceptModalOpen, onClose: onAcceptModalClose } = useDisclosure();
-  const { isOpen: rejectModalIsOpen, onOpen: onRejectModalOpen, onClose: onRejectModalClose } = useDisclosure();
+    const { scholarshipid } = useParams()
+    const [applicants, setApplicants] = useState([])
+    const [students, setStudents] = useState<JSX.Element[]>([])
+    const [selectedID, setSelectedID] = useState(0)
 
-  const acceptAction = () => {
-    onAcceptModalOpen();
-  };
+    /* Get all applicant */
+    const urlApplicant = new URL(process.env.REACT_APP_API_URL + "/api/scholarship/user/" + scholarshipid)    
+    const fetchApplicant = async () => {
+        const applicantsResponse = await axios.get(urlApplicant.toString())
+        setApplicants(applicantsResponse.data.data.scholarships)
 
-  const rejectAction = () => {
-    onRejectModalOpen();
-  };
+        /* Fetch the names of the students */
+        const studentPromises = applicants.map(async (applicant: any) => {
+            const studentURL = new URL(process.env.REACT_APP_API_URL + "/api/user/" + applicant.user_id_student)
+            const studentResponse = await axios.get(studentURL.toString())
+            return {
+                user_id: applicant.user_id_student,
+                name: studentResponse.data.data.user.name,
+                email: studentResponse.data.data.user.email,
+                status: applicant.status
+            }
+        })
 
-  const handleAcceptConfirm = () => {
-    toast({
-      title: "Acceptance set",
-      description: "This applicant has been accepted",
-      status: "success",
-      duration: 9000,
-      isClosable: true,
-    });
-    onAcceptModalClose();
-  };
+        const studentNames = await Promise.all(studentPromises)
 
-  const handleRejectConfirm = () => {
-    toast({
-      title: "Rejection set",
-      description: "This applicant has been rejected",
-      status: "success",
-      duration: 9000,
-      isClosable: true,
-    });
-    onRejectModalClose();
-  };
-
-  return (
-    <Box>
-      <Heading>Scholarship Acceptance</Heading>
-      <Stack>
-        {/* Search Bar */}
-        <InputGroup borderRadius={10} size="sm">
-          <InputLeftElement pointerEvents="none" children={<Search2Icon color="gray.600" />} />
-          <Input type="text" borderRadius={10} placeholder="Search Name..." border="1px solid #949494" />
-        </InputGroup>
-      </Stack>
-      <Box>
-        <TableContainer>
-          <Table variant="simple">
-            <Thead>
-              <Tr>
-                <Th>Student Name</Th>
-                <Th>Email</Th>
-                <Th>Action</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>Nomen</Td>
-                <Td>nomen@gmail.com</Td>
+        const studentProp = studentNames.map((student) => (
+            <Tr key={student.user_id}>
+                <Td>{student.name}</Td>
+                <Td>{student.email}</Td>
                 <Td>
-                  <Button variant="ghost" colorScheme="green" leftIcon={<Icon as={CheckIcon} />} onClick={acceptAction}>
-                    Accept
-                  </Button>
-                  <Button variant="ghost" colorScheme="red" leftIcon={<Icon as={CloseIcon} />} onClick={rejectAction}>
-                    Reject
-                  </Button>
+                    {String(student.status) === "waiting" ? (
+                        <>
+                            <Button
+                                variant="ghost"
+                                colorScheme="green"
+                                leftIcon={<Icon as={CheckIcon} />}
+                                onClick={() => acceptAction(student.user_id)}
+                            >
+                                Accept
+                            </Button>
+                            <Button
+                                variant="ghost"
+                                colorScheme="red"
+                                leftIcon={<Icon as={CloseIcon} />}
+                                onClick={() => rejectAction(student.user_id)}
+                            >
+                                Reject
+                            </Button>
+                        </>
+                    ) : (
+                        student.status
+                    )}
                 </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Box>
+            </Tr>
+        ));
 
-      {/* Accept Confirmation Modal */}
-      <Modal isOpen={acceptModalIsOpen} onClose={onAcceptModalClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Confirm Acceptance</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            Are you sure you want to accept this applicant?
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="green" mr={3} onClick={handleAcceptConfirm}>
-              Confirm
-            </Button>
-            <Button variant="ghost" onClick={onAcceptModalClose}>
-              Cancel
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+        setStudents(studentProp)
+    }
 
-      {/* Reject Confirmation Modal */}
-      <Modal isOpen={rejectModalIsOpen} onClose={onRejectModalClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Confirm Rejection</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            Are you sure you want to reject this applicant?
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="red" mr={3} onClick={handleRejectConfirm}>
-              Confirm
-            </Button>
-            <Button variant="ghost" onClick={onRejectModalClose}>
-              Cancel
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </Box>
-  );
-};
+    useEffect(() => {
+        fetchApplicant()
+    }, [scholarshipid]);
 
-export default Acceptance;
+    const toast = useToast();
+    const { isOpen: acceptModalIsOpen, onOpen: onAcceptModalOpen, onClose: onAcceptModalClose } = useDisclosure();
+    const { isOpen: rejectModalIsOpen, onOpen: onRejectModalOpen, onClose: onRejectModalClose } = useDisclosure();
+
+    const acceptAction = (userID: number) => {
+        setSelectedID(userID)
+        onAcceptModalOpen();
+    };
+
+    const rejectAction = (userID: number) => {
+        setSelectedID(userID)
+        onRejectModalOpen();
+    };
+
+    const handleAcceptConfirm = () => {
+        const url = new URL(process.env.REACT_APP_API_URL + `/api/scholarship/acceptance/${scholarshipid}`)
+        console.log(url.toString())
+        axios.post(url.toString(), {
+            "status": "accepted",
+            "user_id": Number(selectedID)
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+        console.log(error);
+        });
+
+        toast({
+        title: "Acceptance set",
+        description: "This applicant has been accepted",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+        });
+        onAcceptModalClose();
+    };
+
+    const handleRejectConfirm = () => {
+        const url = new URL(process.env.REACT_APP_API_URL + `/api/scholarship/acceptance/${scholarshipid}/accept`)
+        axios.post(url.toString(), {
+            "status": "rejected",
+            "user_id": Number(selectedID)
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+        console.log(error);
+        });
+        toast({
+        title: "Rejection set",
+        description: "This applicant has been rejected",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+        });
+        onRejectModalClose();
+    };
+
+    return (
+        <Box>
+        <Heading>Scholarship Acceptance</Heading>
+        <Stack>
+            {/* Search Bar */}
+            <InputGroup borderRadius={10} size="sm">
+            <InputLeftElement pointerEvents="none" children={<Search2Icon color="gray.600" />} />
+            <Input type="text" borderRadius={10} placeholder="Search Name..." border="1px solid #949494" />
+            </InputGroup>
+        </Stack>
+        <Box>
+            <TableContainer>
+                <Table variant="simple">
+                    <Thead>
+                    <Tr>
+                        <Th>Student Name</Th>
+                        <Th>Email</Th>
+                        <Th>Status</Th>
+                    </Tr>
+                    </Thead>
+                    <Tbody>
+                        {students}
+                    </Tbody>
+                </Table>
+            </TableContainer>
+        </Box>
+
+        {/* Accept Confirmation Modal */}
+        <Modal isOpen={acceptModalIsOpen} onClose={onAcceptModalClose}>
+            <ModalOverlay />
+            <ModalContent>
+            <ModalHeader>Confirm Acceptance</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+                Are you sure you want to accept this applicant?
+            </ModalBody>
+            <ModalFooter>
+                <Button colorScheme="green" mr={3} onClick={handleAcceptConfirm}>
+                Confirm
+                </Button>
+                <Button variant="ghost" onClick={onAcceptModalClose}>
+                Cancel
+                </Button>
+            </ModalFooter>
+            </ModalContent>
+        </Modal>
+
+        {/* Reject Confirmation Modal */}
+        <Modal isOpen={rejectModalIsOpen} onClose={onRejectModalClose}>
+            <ModalOverlay />
+            <ModalContent>
+            <ModalHeader>Confirm Rejection</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+                Are you sure you want to reject this applicant?
+            </ModalBody>
+            <ModalFooter>
+                <Button colorScheme="red" mr={3} onClick={handleRejectConfirm}>
+                Confirm
+                </Button>
+                <Button variant="ghost" onClick={onRejectModalClose}>
+                Cancel
+                </Button>
+            </ModalFooter>
+            </ModalContent>
+        </Modal>
+        </Box>
+    );
+    };
+
+    export default Acceptance;
